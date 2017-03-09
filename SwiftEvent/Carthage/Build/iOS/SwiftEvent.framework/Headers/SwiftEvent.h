76632682e1257c6e135f8381f14ab085929cfd92
l9y <zhiyuannice@icloud.com>
2017-03-09 21:48:46 +0800

    Add example project

---
+++ SwiftEvent/Carthage/Build/iOS/SwiftEvent.framework/Headers/SwiftEvent.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEvent.h
+//  SwiftEvent
+//
+//  Created by zhiyuan wang on 17/3/9.
+//  Copyright © 2017年 l9y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftEvent.
+FOUNDATION_EXPORT double SwiftEventVersionNumber;
+
+//! Project version string for SwiftEvent.
+FOUNDATION_EXPORT const unsigned char SwiftEventVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEvent/PublicHeader.h>
+
+
