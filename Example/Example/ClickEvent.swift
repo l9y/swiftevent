520c2dd418a3a7f996104ae1a954631d6f9ba959
l9y <zhiyuannice@icloud.com>
2017-03-09 21:46:56 +0800

    Add example project

---
+++ Example/Example/ClickEvent.swift
@@ -0,0 +1,14 @@
+//
+//  ClickEvent.swift
+//  Example
+//
+//  Created by zhiyuan wang on 17/3/9.
+//  Copyright © 2017年 l9y. All rights reserved.
+//
+
+import UIKit
+
+class ClickEvent: NSObject {
+
+    
+}
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by zhiyuan wang on 17/3/9.
+//  Copyright © 2017年 l9y. All rights reserved.
+//
+
+import UIKit
+import SwiftEvent
+
+class ViewController: UIViewController {
+
+    let clickEventObserver = Observer<ClickEvent>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        clickEventObserver.receive = {[weak self] clickEvent in
+            self?.onReceiveClickEvent(clickEvent)
+        }
+    }
+
+
+    @IBAction func sendClickEvent(sender: AnyObject) {
+        Event.instance.post(ClickEvent())
+    }
+    
+    
+    func onReceiveClickEvent(clickEvent: ClickEvent) {
+        print("click \(clickEvent)")
+    }
+
+}
+
